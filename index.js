cbcd0a1d229f43b75b3d0f9da14af637e61c3a37
Uzma Ilyas <uzma.paec@gmail.com>
2024-06-03 22:06:31 +0500

    add some attribute of packagejson file

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+console.log("\n***Number Guessing Game***");
+console.log("__________________________\n");
+import inquirer from "inquirer";
+const compno = Math.floor(Math.random() * 10);
+const answergame = await inquirer.prompt([
+    {
+        name: "Userno",
+        message: "Please Guess any Number from 0 to 10:",
+        type: "number",
+    }
+]);
+if (answergame.Userno === compno) {
+    console.log("consgratulation, You Win the game");
+}
+else {
+    console.log(`you lost the game. Right number is ${compno} \n `);
+    console.log("-------- Good luck for next -------\n");
+    console.log("***GAME END***");
+}
